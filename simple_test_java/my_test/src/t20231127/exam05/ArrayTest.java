06dd4ff41dab4a23e5f968d57176a7db211a85b8
GyuminGomin <ool9898@naver.com>
2023-11-27 23:12:12 +0900

    mytest

--- simple_test_java/my_test/src/t20231127/exam05/ArrayTest.java
+++ simple_test_java/my_test/src/t20231127/exam05/ArrayTest.java
@@ -12,31 +12,44 @@ public class ArrayTest<E>{
 
     public ArrayTest() {}
 
-    public ArrayTest(int length) {
-        this.length = length;
+    public ArrayTest(int len) {
+        array = (E[])new Object[len];
+        this.capacity = len;
     }
 
-    public <E> void add(E e) {
-        Arrays.fill(array, e);
+    public void add(E e) {
+        length += 1;
+        if (capacity < length) {
+            array = Arrays.copyOf(array, length);
+            capacity = length;
+        }
+        array[length-1] = e;
     }
 
-    public <E> void remove(E e) {
-        // TODO
-        Arrays.binarySearch(array, e);
+    public void remove(E e) {
+        for(int i=0; i<capacity; i++) {
+            if (array[i].equals(e)) {
+                for (int j = i; j>0; j--) {
+                    E tmp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = tmp;
+                } // 인덱스를 가장 앞으로 보낸 후, 
+                array = Arrays.copyOfRange(array, 1, length);
+                break;
+            }
+        }
+        length--;
+        capacity = length;
     }
 
     @Override
     public String toString() {
-        return "ArrayTest [capacity=" + capacity + ", array=" + Arrays.toString(array) + ", length=" + length + "]";
+        return Arrays.toString(array);
     }
 
     public int size() {
-        return array.length;
+        return length;
     }
-
-    
-
-    
 }
 
 
