a61edbad8f70d0d6f440f204799ed9eee8805102
GyuminGomin <ool9898@naver.com>
2023-12-04 10:20:18 +0900

    pratice5 answer

---
+++ simple_test_java/my_test/src/t20231130/practice5_answer/JavaApplication.java
@@ -0,0 +1,10 @@
+package simple_test_java.my_test.src.t20231130.practice5_answer;
+/**
+ * Application 실행 class
+ * API class와 구분 
+ */ 
+public class JavaApplication {
+	public static void main(String[] args) {
+		new MemberManagement();
+	}
+}
