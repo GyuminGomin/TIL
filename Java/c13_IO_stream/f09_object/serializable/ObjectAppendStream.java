cf1ba79ce0374f98c291d34d438e8bf480850599
GyuminGomin <ool9898@naver.com>
2023-12-08 09:07:42 +0900

    must have to restudying

---
+++ Java/c13_IO_stream/f09_object/serializable/ObjectAppendStream.java
@@ -0,0 +1,17 @@
+package Java.c13_IO_stream.f09_object.serializable;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class ObjectAppendStream extends ObjectOutputStream{
+
+	public ObjectAppendStream(OutputStream out) throws IOException {
+		super(out);
+	}
+
+	@Override
+	protected void writeStreamHeader() throws IOException {
+	}
+	
+}
