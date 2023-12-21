50ad7f5a538798c17002c31cc9fbf8accaa7e610
GyuminGomin <ool9898@naver.com>
2023-12-21 12:22:46 +0900

    bundle, listener_binding

--- workspace_javafx/fx06_event_controller_bundle/e02_bundle/AppMain.java
+++ workspace_javafx/fx06_event_controller_bundle/e02_bundle/AppMain.java
@@ -1,5 +1,9 @@
 package workspace_javafx.fx06_event_controller_bundle.e02_bundle;
 
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,20 +11,50 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class AppMain extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-             HBox root = (HBox)FXMLLoader.load(
-                getClass().getResource("Bundle.fxml")
-            );
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		
+		System.out.println(Locale.getDefault());
+		// Locale 각 나라 지역의[언어][나라] 등의 공통 정보를 담고 있는 class
+		// getAvailableLocales() : 사용 가능한 국가별 언어 코드 정보
+		for(Locale locale : Locale.getAvailableLocales()) {
+			// 한글 언어 이름
+			System.out.printf("Display Language : %s, ", locale.getDisplayLanguage());
+			// 언어 코드
+			System.out.printf("Language Code : %s, ", locale.getLanguage());
+			// 국가 이름
+			System.out.printf("Country : %s, ", locale.getDisplayCountry());
+			// 국가 코드, 국가/언어코드
+			System.out.printf("Country Code : %s, code %s %n", locale.getCountry(), locale.toString());
+		}
+		
+		System.out.println("======================================");
+		Locale locale = Locale.getDefault();
+		System.out.println(locale);
+		Locale localeJP = new Locale("ja","JP");
+		System.out.println(localeJP);
+		Locale localeCN = new Locale("zh","CN");
+		System.out.println(localeCN);
+		Locale.setDefault(new Locale("en","US")); // en_US
+		
+		// prop.s (prop 패키지의 접두어가 s인 녀석) -> prop.s_ko_KR.properties 파일을 읽어옴
+		ResourceBundle bundle = ResourceBundle.getBundle("workspace_javafx/fx06_event_controller_bundle/prop.s", new Locale("ko", "KR"));
+		
+		bundle = ResourceBundle.getBundle("workspace_javafx/fx06_event_controller_bundle/prop.s"); // Locale.setDefault 설정되어 있는 언어 국가코드 파일을 읽어옴
+		Locale.setDefault(localeCN);
+		bundle = ResourceBundle.getBundle("workspace_javafx/fx06_event_controller_bundle/prop.s");
+		
+		try {
+			HBox root = (HBox)FXMLLoader.load(getClass().getResource("Bundle.fxml"),bundle);
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- workspace_javafx/fx07_listener_binding/l01_property_listener/Main.java
+++ workspace_javafx/fx07_listener_binding/l01_property_listener/Main.java
@@ -1,26 +1,26 @@
 package workspace_javafx.fx07_listener_binding.l01_property_listener;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
 
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            BorderPane root = (BorderPane)FXMLLoader.load(
-                getClass().getResource("")
-            );
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Root.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
